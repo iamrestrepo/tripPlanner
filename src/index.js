921c3dc0a2de7e83d226537b5f91d2f8a068e62d
Monique <moniquerz@Moniques-MacBook-Pro.local>
2018-05-01 17:14:47 -0400

    adds not important stuff

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ const mapboxgl = require("mapbox-gl");
 mapboxgl.accessToken = "pk.eyJ1IjoibW9uaXF1ZXJ6IiwiYSI6ImNqZ280ZjluNjFjdXQyd3JtcXRtcXV4NWkifQ.MBYun0SJwf-BVIs3ESzd8w";
 
 const map = new mapboxgl.Map({
-  container: "map",
-  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
-  zoom: 12, // starting zoom
-  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+    container: "map",
+    center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+    zoom: 12, // starting zoom
+    style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
 
 
-
 console.log('this is  index.js!');
+
