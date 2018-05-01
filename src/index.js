82893bd7b77fa32eb4c62e7e205a4937881c9e47
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-05-01 17:11:21 -0400

    adds starting marker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-const mapboxgl = require("mapbox-gl");
+import { Marker } from "mapbox-gl";
 
+const mapboxgl = require("mapbox-gl");
+const markerEle = document.createElement("div")
+markerEle.style.width = "32px";
+markerEle.style.height = "39px";
+markerEle.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
 
 mapboxgl.accessToken = "pk.eyJ1IjoibW9uaXF1ZXJ6IiwiYSI6ImNqZ280ZjluNjFjdXQyd3JtcXRtcXV4NWkifQ.MBYun0SJwf-BVIs3ESzd8w";
 
@@ -10,6 +15,6 @@ const map = new mapboxgl.Map({
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
 
-
+new mapboxgl.Marker(markerEle).setLngLat([-74.009, 40.705]).addTo(map)
 
 console.log('this is  index.js!');
